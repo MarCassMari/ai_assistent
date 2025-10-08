32e27a38f10d79927a7179150bb6fa6b3c41d50d
MarCassMari <marcassmachado32@gmail.com>
2025-10-07 21:33:44 -0300

    feat/llm-basico

--- main.py
+++ main.py
@@ -2,9 +2,23 @@ from dotenv import load_dotenv
 from pydanic import BaseModel
 from langchain_openai import ChatOpenAI
 from langchain_anthropic import ChatAnthropic
+from langchain_core.prompts import ChataPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
 
 load_dotenv()
+
+class PoemaResponse(BaseModel):
+    poema: str
+    autor: str
+    tema: str
+    data_de_criacao: str
+
 
 # Initialize LLMs - choose one or both - depending on your use case
 llm = ChatOpenAI(model = "gpt-4o-mini", temperature=0)
 llmTwo = ChatAnthropic(model = "claude-4", temperature=0) 
+
+response =llm.invoke("Escreva um poema de amor para minha namorada.")
+print(response)
+
+
